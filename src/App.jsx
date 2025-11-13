cd949483217bcd61f4953f11f5250bdce9822d45
BanLowi <lowi40k@gmail.com>
2025-11-13 16:15:59 +0100

    state variable and first name and last name inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,54 @@
+import { useState } from "react"
+
 function App() {
+
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: ""
+  });
 
 
   return (
     <>
+
+      <header><h1>FORM POST</h1></header>
+
+      <main>
+
+        <div className="container">
+
+          <form>
+
+            <div class="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                name=""
+                id=""
+                aria-describedby="helpId"
+                placeholder="First Name"
+              />
+            </div>
+
+            <div class="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                name=""
+                id=""
+                aria-describedby="helpId"
+                placeholder="Last Name"
+              />
+            </div>
+
+
+
+          </form>
+
+        </div>
+
+
+      </main>
 
     </>
   )
